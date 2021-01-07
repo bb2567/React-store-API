efd7d905cb17b8740fd5fab1b1eb91632dbfc623
bb2567 <marksong0976@gmail.com>
2021-01-07 19:36:02 +0800

    Json Server 添加驗證

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const fs = require("fs");
 const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middleWares = jsonServer.defaults();
+
 server.use(jsonServer.bodyParser);
 server.use(middleWares);
 
@@ -13,25 +15,34 @@ const getUsersDb = () => {
     fs.readFileSync(path.join(__dirname, "users.json"), "UTF-8")
   );
 };
-
-
-//   傳遞的資料是否與 users.json 符合
+//   驗證
 const isAuthenticated = ({ email, password }) => {
   return (
-  getUsersDb().users.findIndex(
-      user => user.email === email && user.password === password
+    getUsersDb().users.findIndex(
+      (user) => user.email === email && user.password === password
     ) !== -1
   );
 };
+// Jwt 註冊
+const SECRET = "12321JKLSJKLSDFJK23423432";
+const expiresIn = "1h";
+const createToken = (payload) => {
+  return jwt.sign(payload, SECRET, { expiresIn });
+};
 
-
-
-
+//   資料 與 users驗證
 server.post("/auth/login", (req, res) => {
   const { email, password } = req.body;
 
   if (isAuthenticated({ email, password })) {
-    const jwToken = "sfsfsgsghhhsd.1454564dsdsfggg.sfsf44";
+    const user = getUsersDb().users.find(
+      (u) => u.email === email && u.password === password
+    );
+
+    const { nickname, type } = user;
+
+    //   jwt
+    const jwToken = createToken({ nickname, type });
     return res.status(200).json(jwToken);
   } else {
     const status = 401;
